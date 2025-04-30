4cb8cf49778d7db8a50c21fef6d7a21c4956a82b
Helena Avis <helenaavis48@gmail.com>
2025-04-30 16:38:48 +0100

    Created projects JSON file and functions to search and create project cards.

---
+++ scripts/projectsSearch.js
@@ -0,0 +1,55 @@
+const searchInput = document.querySelector('.search-bar');
+searchInput.addEventListener('keyup', function (e) {
+    if (searchInput.value.length > 0) {
+        search(searchInput.value);
+    }
+});
+const projectsContainer = document.querySelector('.projects-container');
+
+// Function to fetch projects data and filter based on search query
+function search(query) {
+    fetch('../data/projects.json')
+        .then((response) => response.json())
+        .then((projects) => {
+            console.log('Loaded projects data:', projects);
+            projects.forEach((project) => {
+                if (project.name.toLowerCase().includes(query.toLowerCase())) {
+                    buildProjectCard(project);
+                }
+            });
+        });
+}
+
+// Function to build project cards based on the provided project JSON
+function buildProjectCard(project) {
+    console.log('Building project card for:', project.name);
+
+    // Create a new project card element
+    const projectCard = document.createElement('div');
+    projectCard.classList.add('project-card');
+    // create the card title (project name)
+    const cardName = document.createElement('h2');
+    cardName.classList.add('project-card__name');
+    cardName.textContent = project.name;
+    // create the project description
+    const projectDescription = document.createElement('p');
+    projectDescription.classList.add('project-card__description');
+    projectDescription.textContent = project.description;
+    // create the github link if present
+    if (project.githubURL) {
+        const githubLink = document.createElement('a');
+        githubLink.classList.add('project-card__link');
+        githubLink.href = project.github;
+        githubLink.textContent = 'Github Repo';
+    }
+    // create the live demo link if present
+    if (project.liveURL) {
+        const liveLink = document.createElement('a');
+        liveLink.classList.add('project-card__link');
+        liveLink.href = project.liveURL;
+        liveLink.textContent = 'Live Project';
+    }
+
+    // add project card to list
+    projectsContainer.appendChild(projectCard);
+}
